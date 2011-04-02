6610b8e79098b48e6a79d2ad4545c665068162e4
Vladyslav Lubenets <vladyslavu@gmail.com>
2011-04-03 01:23:54 +0300

    Added network module

---
+++ src/vladyslav/lubenets/nac/network/SocketWrapper.java
@@ -0,0 +1,49 @@
+package vladyslav.lubenets.nac.network;
+
+import vladyslav.lubenets.nac.game.Game.Result;
+
+public class SocketWrapper {
+
+    public enum State {
+        OPEN, CLOSED;
+    }
+
+
+    public Result connect(String string, int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    public Result create(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+
+    public Result write(String string) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    public String read() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+
+    public State getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
