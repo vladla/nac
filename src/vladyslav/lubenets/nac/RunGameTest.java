bd674dee0a363e2a35bff8d0211a8e777213afd5
Vladyslav Lubenets <vladyslavu@gmail.com>
2011-04-03 00:26:11 +0300

    Cutted network tests

--- src/vladyslav/lubenets/nac/RunGameTest.java
+++ src/vladyslav/lubenets/nac/RunGameTest.java
@@ -2,20 +2,15 @@ package vladyslav.lubenets.nac;
 
 import vladyslav.lubenets.nac.Game.Player;
 import vladyslav.lubenets.nac.Game.Result;
-import vladyslav.lubenets.nac.ServerSide.State;
 import junit.framework.TestCase;
 
 public class RunGameTest extends TestCase {
 
 	Game game;
-	ServerSide serverSide; 
-    ClientSide clientSide;
 	
 	@Override
     public void setUp() {
 		game = new GameImplement();
-		serverSide = new ServerSide();
-        clientSide = new ClientSide();
 	}
 
 	public void testCrossesWin() {
@@ -264,125 +259,5 @@ public class RunGameTest extends TestCase {
         
     }
 	
-	// Server side testing
-	
-	public void testServerCreateSocket() {
-	    Result result = serverSide.create("127.0.0.1", 1234);
-	    assertEquals(result, Result.SUCCESS);
-	    
-	    ServerSide.State state = serverSide.getState();
-	    assertEquals(state, State.OPEN);
-	    
-	    serverSide.close();
-	    
-	    state = serverSide.getState();
-	    assertEquals(state, State.CLOSED);
-        	    
-	}
-
-    public void testServerTestConnection() {
-        Result result = serverSide.test("The string for testing...");
-        assertEquals(result, Result.SUCCESS);
-
-        
-        ServerSide.State state = serverSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        serverSide.close();
-        
-        state = serverSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-    
-    public void testServerWritting() {
-        Result result = serverSide.write("The string for writting...");
-        assertEquals(result, Result.SUCCESS);
-
-        
-        ServerSide.State state = serverSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        serverSide.close();
-        
-        state = serverSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-
-    public void testServerReading() {
-        Result result = serverSide.read();
-        assertEquals(result, Result.SUCCESS);
-        
-        
-        ServerSide.State state = serverSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        serverSide.close();
-        
-        state = serverSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-    
-    //Client side testing
-
-    public void testClientEstablishSocketConnection() {
-        Result result = clientSide.connect("127.0.0.1", 1234);
-        assertEquals(result, Result.SUCCESS);
-        
-        
-        ClientSide.State state = clientSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        clientSide.close();
-        
-        state = clientSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-
-    public void testClientTestConnection() {
-        Result result = clientSide.test("The string for testing...");
-        assertEquals(result, Result.SUCCESS);
-        
-        ClientSide.State state = clientSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        clientSide.close();
-        
-        state = clientSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-    
-    public void testClientWritting() {
-        Result result = clientSide.write("The string for writting...");
-        assertEquals(result, Result.SUCCESS);
-
-        ClientSide.State state = clientSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        clientSide.close();
-        
-        state = clientSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-
-    public void testClientReading() {
-        Result result = clientSide.read();
-        assertEquals(result, Result.SUCCESS);
-
-        ClientSide.State state = clientSide.getState();
-        assertEquals(state, State.OPEN);
-        
-        clientSide.close();
-        
-        state = clientSide.getState();
-        assertEquals(state, State.CLOSED);
-
-    }
-    
     
 }
