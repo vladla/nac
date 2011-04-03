9f7d5396fcd4bc1f7c2cec9866804be5f0f265d1
Vladyslav Lubenets <vladyslavu@gmail.com>
2011-04-03 23:10:21 +0300

    FTP connection throw Socket

--- src/vladyslav/lubenets/nac/network/SocketWrapper.java
+++ src/vladyslav/lubenets/nac/network/SocketWrapper.java
@@ -1,142 +1,163 @@
 package vladyslav.lubenets.nac.network;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.SocketAddress;
+
 
 public class SocketWrapper {
-    Socket clientSocket;
-    ServerSocket serverSocket;
-    PrintWriter out = null;
+//    Socket clientSocket;
+//    ServerSocket serverSocket;
+//    PrintWriter out = null;
     BufferedReader in = null;
-    boolean flagServerCreated = false;
-    
-    
-    int portNumber;
-    
-    public enum State {
-        OPEN, CLOSED;
-    }
+//    boolean flagServerCreated = false;
+//    
+//    
+//    int portNumber;
+//    
+//    public enum State {
+//        OPEN, CLOSED;
+//    }
+//
+//    public enum Result {
+//        SUCCESS, FAIL, ERROR
+//    }
+//
+//    public Result connect(String host, @SuppressWarnings("hiding") int portNumber) {
+//        
+//       try {
+//           if(flagServerCreated==true){
+//               return Result.FAIL;
+//           }
+//        clientSocket = new Socket(host, portNumber);
+//    } catch (UnknownHostException ex) {
+//        ex.printStackTrace();
+//    } catch (IOException ex) {
+//        ex.printStackTrace();
+//        return Result.FAIL;
+//    }
+//        
+//        return Result.SUCCESS;
+//    }
+//
+//    public Result create(int i) {
+//        try {
+//            serverSocket = new ServerSocket(i);
+//            flagServerCreated = true;
+//        } catch (IOException e) {
+//            return Result.ERROR;
+//        }
+//        return Result.SUCCESS;
+//    }
+//
+//
+//    
+//    public State getState() {
+//        if (!(serverSocket.isClosed())) {
+//            return State.OPEN;
+//        } return State.CLOSED;
+//        
+//    }
+//
+//    public void close() {
+//
+//        try {
+//            if (!(serverSocket.isClosed())) {
+//                serverSocket.close();
+//            }
+//            if (!(clientSocket.isClosed())) {
+//                clientSocket.close();
+//            }
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//    }
+//
+//
+//    public String readByClient() {
+//        try {
+//            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            if (in.equals(NetworkTest.STRING_TO_WRITE)) {
+//                return NetworkTest.STRING_TO_WRITE;
+//            }
+//        } catch (IOException ex) {
+//            // TODO Auto-generated catch block
+//            ex.printStackTrace();
+//        }
+//        return null;
+//    }
+//
+//    public Result writeByClient(String stringToWrite) {
+//        
+//        try {
+//            out = new PrintWriter(clientSocket.getOutputStream(), true);
+//            out.write(stringToWrite);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        
+//        if (out.equals(null)) {
+//            return Result.SUCCESS;
+//        }
+//            return Result.FAIL;
+//    }
+//    
+//    public String readByServer() {
+//        try {
+//            in = new BufferedReader(new InputStreamReader(serverSocket.accept().getInputStream()));
+//            if (in.equals(NetworkTest.STRING_TO_WRITE)) {
+//                return NetworkTest.STRING_TO_WRITE;
+//            }
+//        } catch (IOException ex) {
+//            // TODO Auto-generated catch block
+//            ex.printStackTrace();
+//        }
+//        return null;
+//    }
+//    
+//    
+//    public Result writeByServer(String stringToWrite) {
+//    
+//        try {
+//            out = new PrintWriter(serverSocket.accept().getOutputStream(), true);
+//            out.write(stringToWrite);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        
+//        if (out.equals(null)) {
+//            return Result.SUCCESS;
+//        }
+//            return Result.FAIL;
+//    }
 
-    public enum Result {
-        SUCCESS, FAIL, ERROR
-    }
-
-    public Result connect(String host, @SuppressWarnings("hiding") int portNumber) {
-        
-       try {
-           if(flagServerCreated==true){
-               return Result.FAIL;
-           }
-        clientSocket = new Socket(host, portNumber);
-    } catch (UnknownHostException ex) {
-        ex.printStackTrace();
-    } catch (IOException ex) {
-        ex.printStackTrace();
-        return Result.FAIL;
-    }
-        
-        return Result.SUCCESS;
-    }
-
-    public Result create(int i) {
+    public String stringFromTheFtp() {
         try {
-            serverSocket = new ServerSocket(i);
-            flagServerCreated = true;
+            InetAddress inetAddress = InetAddress.getByName("ftp.dlink.com");
+            int port = 21;
+            SocketAddress socketAddress = new InetSocketAddress(inetAddress, port);
+            
+            Socket socket = new Socket();
+            int timeoutMs = 2000;   
+            socket.connect(socketAddress, timeoutMs);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));           
+            String returnFromFtp = in.readLine();
+            return returnFromFtp;
+            
         } catch (IOException e) {
-            return Result.ERROR;
-        }
-        return Result.SUCCESS;
-    }
-
-
-    
-    public State getState() {
-        try {
-            if (!(serverSocket.accept().isClosed())) {
-                return State.OPEN;
-            }
-        } catch (IOException ex) {
-            // TODO Auto-generated catch block
-            ex.printStackTrace();
-        } return State.CLOSED;
-        
-    }
-
-    public void close() {
-
-        try {
-            serverSocket.close();
-            clientSocket.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-    }
-
-
-    public String readByClient() {
-        try {
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            if (in.equals(NetworkTest.STRING_TO_WRITE)) {
-                return NetworkTest.STRING_TO_WRITE;
-            }
-        } catch (IOException ex) {
-            // TODO Auto-generated catch block
-            ex.printStackTrace();
+            System.out.println("");
         }
         return null;
-    }
 
-    public Result writeByClient(String stringToWrite) {
         
-        try {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.write(stringToWrite);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
         
-        if (out.equals(null)) {
-            return Result.SUCCESS;
-        }
-            return Result.FAIL;
     }
-    
-    public String readByServer() {
-        try {
-            in = new BufferedReader(new InputStreamReader(serverSocket.accept().getInputStream()));
-            if (in.equals(NetworkTest.STRING_TO_WRITE)) {
-                return NetworkTest.STRING_TO_WRITE;
-            }
-        } catch (IOException ex) {
-            // TODO Auto-generated catch block
-            ex.printStackTrace();
-        }
-        return null;
-    }
-    
-    
-    public Result writeByServer(String stringToWrite) {
-    
-        try {
-            out = new PrintWriter(serverSocket.accept().getOutputStream(), true);
-            out.write(stringToWrite);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        
-        if (out.equals(null)) {
-            return Result.SUCCESS;
-        }
-            return Result.FAIL;
-    }
-
-
 
 
 }
--- test/vladyslav/lubenets/nac/network/NetworkTest.java
+++ test/vladyslav/lubenets/nac/network/NetworkTest.java
@@ -1,88 +1,93 @@
 package vladyslav.lubenets.nac.network;
 
 import junit.framework.TestCase;
-import vladyslav.lubenets.nac.network.SocketWrapper.Result;
-import vladyslav.lubenets.nac.network.SocketWrapper.State;
+//import vladyslav.lubenets.nac.network.SocketWrapper.Result;
+//import vladyslav.lubenets.nac.network.SocketWrapper.State;
 
 
 public class NetworkTest extends TestCase {
-    public static final String STRING_TO_WRITE = "Some information to transport";
+//    public static final String STRING_TO_WRITE = "Some information to transport";
     SocketWrapper socketWrapperServer; 
-    SocketWrapper socketWrapperClient; 
-    
+    public static final String FTP_RESULT = "220 (vsFTPd 1.2.1)";
+//    SocketWrapper socketWrapperClient; 
+//    
     @Override
     public void setUp() {
         socketWrapperServer = new SocketWrapper();
-        socketWrapperClient = new SocketWrapper();        
+//        socketWrapperClient = new SocketWrapper();        
     }
-
-    @Override
-    public void tearDown() {
-        socketWrapperServer.close();
-        socketWrapperClient.close();
-        
-    }    
-   
+//
+//    @Override
+//    public void tearDown() {
+//        socketWrapperServer.close();
+//        socketWrapperClient.close();
+//        
+//    }    
+//   
+//    
+//    public void testServerCreateSocket() {
+//        Result result = socketWrapperServer.create(1234);
+//        assertEquals(result, Result.SUCCESS);
+//        
+//        result = socketWrapperServer.connect("127.0.0.1", 1234);
+//        assertEquals(result, Result.FAIL);
+//                
+//        SocketWrapper.State state = socketWrapperServer.getState();
+//        assertEquals(state, State.OPEN);
+//        
+//        socketWrapperServer.close();
+//        
+//        state = socketWrapperServer.getState();
+//        assertEquals(state, State.CLOSED);
+//                
+//    }
+//
+//
+//    public void testSocketReading() {
+//        
+//        Result result = socketWrapperServer.create(1234);
+//        assertEquals(result, Result.SUCCESS);
+//        
+//        result = socketWrapperClient.connect("127.0.0.1", 1234);
+//        assertEquals(result, Result.SUCCESS);
+//        
+//
+//        result = socketWrapperClient.writeByClient(STRING_TO_WRITE);
+//        assertEquals(result, Result.SUCCESS);
+//
+//        String resultString = socketWrapperServer.readByServer();
+//        assertEquals(resultString, STRING_TO_WRITE);
+//
+//        result = socketWrapperServer.writeByServer(STRING_TO_WRITE);
+//        assertEquals(result, Result.SUCCESS);
+//        
+//        resultString = socketWrapperClient.readByClient();
+//        assertEquals(resultString, STRING_TO_WRITE);
+//        
+//        socketWrapperServer.close();
+//        assertEquals(socketWrapperServer.getState(), State.CLOSED);
+//
+//
+//        socketWrapperClient.close();
+//        assertEquals(socketWrapperClient.getState(), State.CLOSED);
+//
+//    }
+//
+//    public void testClientCreationFaild() {
+//        Result result = socketWrapperServer.create(1234);
+//        assertEquals(result, Result.SUCCESS);
+//                
+//        result = socketWrapperClient.connect("127.0.0.1", 1234);
+//        assertEquals(result, Result.SUCCESS);
+//        
+//        result = socketWrapperClient.create(1234);
+//        assertEquals(result, Result.FAIL);
+//                
+//    }
     
-    public void testServerCreateSocket() {
-        Result result = socketWrapperServer.create(1234);
-        assertEquals(result, Result.SUCCESS);
-        
-        result = socketWrapperServer.connect("127.0.0.1", 1234);
-        assertEquals(result, Result.FAIL);
-                
-        SocketWrapper.State state = socketWrapperServer.getState();
-        assertEquals(state, State.OPEN);
-        
-        socketWrapperServer.close();
-        
-        state = socketWrapperServer.getState();
-        assertEquals(state, State.CLOSED);
-                
-    }
-
-
-    public void testSocketReading() {
-        
-        Result result = socketWrapperServer.create(1234);
-        assertEquals(result, Result.SUCCESS);
-        
-        result = socketWrapperClient.connect("127.0.0.1", 1234);
-        assertEquals(result, Result.SUCCESS);
-        
-
-        result = socketWrapperClient.writeByClient(STRING_TO_WRITE);
-        assertEquals(result, Result.SUCCESS);
-
-        String resultString = socketWrapperServer.readByServer();
-        assertEquals(resultString, STRING_TO_WRITE);
-
-        result = socketWrapperServer.writeByServer(STRING_TO_WRITE);
-        assertEquals(result, Result.SUCCESS);
-        
-        resultString = socketWrapperClient.readByClient();
-        assertEquals(resultString, STRING_TO_WRITE);
-        
-        socketWrapperServer.close();
-        assertEquals(socketWrapperServer.getState(), State.CLOSED);
-
-
-        socketWrapperClient.close();
-        assertEquals(socketWrapperClient.getState(), State.CLOSED);
-
-    }
-
     public void testClientCreationFaild() {
-        Result result = socketWrapperServer.create(1234);
-        assertEquals(result, Result.SUCCESS);
-                
-        result = socketWrapperClient.connect("127.0.0.1", 1234);
-        assertEquals(result, Result.SUCCESS);
-        
-        result = socketWrapperClient.create(1234);
-        assertEquals(result, Result.FAIL);
-                
+        String result = socketWrapperServer.stringFromTheFtp();
+        assertEquals(result, FTP_RESULT);
     }
-    
 
 }
